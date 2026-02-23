14e26c6644c8b023da2d00a9e2844cc89c2ccc33
sujal goel <sujalgoel19@gmail.com>
2026-02-23 18:40:59 +0530

    multer config added

--- backend/routes/plan.js
+++ backend/routes/plan.js
@@ -18,7 +18,7 @@ if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir, { recursive: true })
 }
 
-const storage = multer.diskStorage({
+const storage = multer.memoryStorage({
   destination: (_req, _file, cb) => {
     cb(null, uploadsDir)
   },
@@ -36,7 +36,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage,
   limits: {
-    fileSize: 20 * 1024 * 1024,
+    fileSize: 4 * 1024 * 1024,
   },
 })
 
